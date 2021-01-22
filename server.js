a6c99686eaebee651eba300803a7c91ec1c8938d
Jonathan Kohen <jonathan.a.kohen@gmail.com>
2021-01-21 19:05:12 -0500

    adjusted file directory

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
+const express = require('express'),
+    app = express(),
+    port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +11,8 @@ app.get('/api', (req, res) => {
     res.send('Yoni was here');
 });
 
-const server = app.listen(process.env.PORT, () =>
-    console.log(`Listening on port ${server.address().port}...`)
+const server = app.listen(port, () =>
+    console.log(`Listening on port ${port}...`)
 );
 
 const io = require('socket.io')(server, { cors: true });
